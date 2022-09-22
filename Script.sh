61790087d858fa85b23e595a7bce4272cbc7ae8f
U-HSC\E02655 <E02655@GGNS1LTE02655.hsc.com>
2022-09-22 20:14:19 +0530

    Adding code

---
+++ Script.sh
@@ -0,0 +1,15 @@
+#! /bin/bash/
+
+aa="Awesome Anshu"
+rr="Rowdy Rupesh"
+Rr="Radient Raghu"
+nn="Naughty Nishant" 
+
+echo "NAD Team"
+
+echo "Date=$(date)"
+
+echo "Manager=$aa"  #AA
+echo "Sr.Principle Engg=$rr" #RR
+echo "Principle Engg=$Rr" #RRR
+echo "Associate Engg=$nn" #NN
